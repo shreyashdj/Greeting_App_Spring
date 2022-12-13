543387140ba622df7034cf9bab009b938a814da9
shreyashdj <shreyashjadhav1799@gmail.com>
2022-12-13 06:37:41 +0530

    [Shreyash]: Initialised Greeting App

---
+++ src/test/java/com/bridgelabz/greeting_app_spring/GreetingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greeting_app_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
